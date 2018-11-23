5291b9916eff20128f7458ce432e4f25b23d35a1
southside <opdort24@gmail.com>
2018-11-23 18:50:25 +0100

    added navigation bar but not completed

--- app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
+++ app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
@@ -12,10 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         uiNewsHome = new WebView(MainActivity.this);
-        setContentView(uiNewsHome);
+        setContentView(R.layout.activity_main);
+
+
+        //get layout and load web contents
+        uiNewsHome = (WebView) findViewById(R.id.webview);
         uiNewsHome.loadUrl("https://www.ui.edu.ng/news");
+
+        //handling navigations within the application
         WebViewClient myWebviewClient = new WebViewClient();
         uiNewsHome.setWebViewClient(myWebviewClient);
+
+
     }
 
     @Override
