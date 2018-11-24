a0e6f2c6f82dfe2fbfbbc472eac100bbfd39e1fe
southside <opdort24@gmail.com>
2018-11-24 21:40:40 +0100

    navigation working with fragment without webview

--- app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
+++ app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.opeyemi.uinews;
 
+import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -11,32 +13,53 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    WebView uiNewsHome;
+    //WebView uiNewsHome;
     BottomNavigationView navigation;
+
+
+    BulletinFragment bulletinFragment;
+    NewsFragment newsFragment;
+    AboutFragment aboutFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        uiNewsHome = new WebView(MainActivity.this);
+        //uiNewsHome = new WebView(MainActivity.this);
         setContentView(R.layout.activity_main);
 
 
         //get layout and load web contents
-        uiNewsHome = (WebView) findViewById(R.id.webview);
-        uiNewsHome.loadUrl("https://www.ui.edu.ng/news");
+        //uiNewsHome = (WebView) findViewById(R.id.webview);
+        //uiNewsHome.loadUrl("https://www.ui.edu.ng/news");
 
         //handling navigations within the application
-        WebViewClient myWebviewClient = new WebViewClient();
-        uiNewsHome.setWebViewClient(myWebviewClient);
+        //WebViewClient myWebviewClient = new WebViewClient();
+        //uiNewsHome.setWebViewClient(myWebviewClient);
 
+        //get fragment instances
+        newsFragment = new NewsFragment();
+        bulletinFragment = new BulletinFragment();
+        aboutFragment = new AboutFragment();
+
+        setFragment(newsFragment);
 
         //get bottom navigation instance and set listener
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         NavigationSelectedListener navigationSelectedListener = new NavigationSelectedListener();
         navigation.setOnNavigationItemSelectedListener(navigationSelectedListener);
 
+
+
     }
 
+
+    //sets the fragment to be displayed
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+    }
+/**
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK) && uiNewsHome.canGoBack()) {
@@ -44,9 +67,10 @@ public class MainActivity extends AppCompatActivity {
         };
         return super.onKeyDown(keyCode, event);
     }
-
+ **/
     //listener interface for the bottom navigation when selected
     private class NavigationSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -55,12 +79,15 @@ public class MainActivity extends AppCompatActivity {
             {
                 case R.id.action_news:
                     Toast.makeText(MainActivity.this, "News was clicked", Toast.LENGTH_SHORT).show();
+                    setFragment(newsFragment);
                     break;
                 case R.id.action_bulletin:
                     Toast.makeText(MainActivity.this, "Bullettin was clicked", Toast.LENGTH_SHORT).show();
+                    setFragment(bulletinFragment);
                     break;
                 case R.id.action_information:
                     Toast.makeText(MainActivity.this, "Information was clicked", Toast.LENGTH_SHORT).show();
+                    setFragment(aboutFragment);
                     break;
             }
             return true;
