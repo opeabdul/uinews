001e686caf918c148d6bd43fce8f0613d1629032
southside <opdort24@gmail.com>
2018-11-25 12:14:06 +0100

    navigation working with uinews webview added

--- app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
+++ app/src/main/java/com/example/opeyemi/uinews/MainActivity.java
@@ -13,7 +13,7 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //WebView uiNewsHome;
+
     BottomNavigationView navigation;
 
 
@@ -24,17 +24,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //uiNewsHome = new WebView(MainActivity.this);
+
         setContentView(R.layout.activity_main);
 
-
-        //get layout and load web contents
-        //uiNewsHome = (WebView) findViewById(R.id.webview);
-        //uiNewsHome.loadUrl("https://www.ui.edu.ng/news");
-
-        //handling navigations within the application
-        //WebViewClient myWebviewClient = new WebViewClient();
-        //uiNewsHome.setWebViewClient(myWebviewClient);
 
         //get fragment instances
         newsFragment = new NewsFragment();
--- app/src/main/java/com/example/opeyemi/uinews/NewsFragment.java
+++ app/src/main/java/com/example/opeyemi/uinews/NewsFragment.java
@@ -2,10 +2,14 @@ package com.example.opeyemi.uinews;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
@@ -13,18 +17,34 @@ import android.view.ViewGroup;
  */
 public class NewsFragment extends Fragment {
 
+    private WebView uiNewsHomepage;
 
     public NewsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        uiNewsHomepage = new WebView(getContext());
 
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        //Get layout and load web contents
+        View newsFragmentLayout = inflater.inflate(R.layout.fragment_news,container,false);
+
+        uiNewsHomepage = (WebView) newsFragmentLayout.findViewById(R.id.webview);
+        uiNewsHomepage.loadUrl("https://www.ui.edu.ng/news");
+        Log.e("NewsFrgment","It runned the loadurl");
+        //Handling navigations within the application
+        WebViewClient myWebviewClient = new WebViewClient();
+        uiNewsHomepage.setWebViewClient(myWebviewClient);
+        return newsFragmentLayout;
     }
 
 }
